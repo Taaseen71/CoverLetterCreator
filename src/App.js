cbee365ea90b191056a449227c9a4f0b944f3507
Saadat Taaseen <taaseen.homework@gmail.com>
2021-02-18 12:57:45 -0500

    replaced alert with forms

--- src/App.js
+++ src/App.js
@@ -6,32 +6,42 @@ function App() {
 
     const [todayDate, setTodayDate] = useState(new Date().toLocaleString().split(","))
     const [positionName, setPositionName] = useState("POSITION NAME");
-    const [extraComments, setExtraComments] = useState("");
     const [companyName, setCompanyName] = useState("COMPANY NAME")
+    const [extraComments, setExtraComments] = useState("");
+    const [statePositionChange, setStatePositionChange] = useState("")
+    const [stateCompanyChange, setStateCompanyChange] = useState("")
+    const [stateExtraComments, setStateExtraComments] = useState("")
+
+
     console.log(todayDate[0])
 
 
-    useEffect(() => {
-        let newPositionName = prompt('enter POSITION NAME')
-        let newCompanyName = prompt('enter COMPANY NAME')
-        // let confirmAdditional = confirm('Any Extra Comments you want to add after the first paragraph?')
-        // let newExtraComments = prompt('Any Extra Comments you want to add after the first paragraph?')
-        let newExtraComments;
 
-        if (window.confirm('Any Extra Comments you want to add after the first paragraph?')) {
-            newExtraComments = prompt('Type Additional Information in box')
-        }
-        else {
-            newExtraComments = null;
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setPositionName(statePositionChange)
+        setCompanyName(stateCompanyChange)
+        setExtraComments(stateExtraComments)
+        setStatePositionChange("")
+        setStateCompanyChange("")
+        setStateExtraComments("")
+    }
 
-        newCompanyName ? (setCompanyName(newCompanyName)) : (setCompanyName("COMPANY NAME"))
-        newPositionName ? (setPositionName(newPositionName)) : (setPositionName("POSITION NAME"))
-        newExtraComments ? (setExtraComments(newExtraComments)) : (setExtraComments(""));
-    }, [])
+    const handlePositionChange = (event) => {
+        setStatePositionChange(event.target.value)
+    }
+    const handleCompanyChange = (event) => {
+        setStateCompanyChange(event.target.value)
+    }
+
+    const handleExtraCommentChange = event => {
+        setStateExtraComments(event.target.value)
+    }
 
 
-    const paragraph1 = `I am writing to express my strong interest in the ${positionName} position at ${companyName}. I am a highly self-motivated Full Stack Software Engineer who exhibits strong analytical and critical thinking skills to write custom HTML, CSS and JavaScript, as well as following semantic W3C coding practices. I have experience in React-Native, React, Node, Ruby On Rails, and have worked with databases such as MongoDB, PostgreSQL and Firebase. I have experience building websites with full CRUD and authentication, and am passionate about coding. I have experience working with UX/UI designers and work well with a team, with minimal supervision. I am also well versed in mobile first and future friendly designs with an advanced knowledge of Git version control and progressive enhancement. I am a very fast learner, and I have a passion towards coding. Any experiences I may have lacking for this position, I can assure you that I will learn very quickly and be able  to apply it to my code as required. ${extraComments}`
+
+
+    const paragraph1 = `I am writing to express my strong interest in the ${positionName} position at ${companyName}. I am a highly self-motivated Full Stack Software Engineer who exhibits strong analytical and critical thinking skills to write custom HTML, CSS and JavaScript, as well as following semantic W3C coding practices. I have experience in React-Native, React, Node, Ruby On Rails, and have worked with databases such as MongoDB, PostgreSQL and Firebase. I have experience building websites with full CRUD and authentication, and am passionate about coding. I have experience working with UX/UI designers and work well with a team, with minimal supervision. I am also well versed in mobile first and future friendly designs with an advanced knowledge of Git version control and progressive enhancement. I am a very fast learner, and I have a passion towards coding. ${extraComments}`
 
     const paragraph2 = `I am a recent graduate from General Assembly as a Software Engineering Fellow. Before joining General Assembly, I worked as a Quality Control Scientist in a Pharmaceutical Company and from there I gathered extensive knowledge on how to extract information through analysis and make reports with proper documentation practices that I am sure will help me in this field of work as well. I have a lot of experience working and collaborating with large groups and work particularly well under pressure. I have learnt that through patience, hard  work and a growth mindset, positive results can always be attainable.`
 
@@ -43,10 +53,27 @@ function App() {
     return (
         <div className="fullPage">
             <header className="header">
-                <p>Saadat Taaseen <br /></p>
+                <form onSubmit={handleSubmit}>
+                    <p className="headerContents">Saadat Taaseen </p>
+                    <label className="headerContents">
+                        Position Name:
+                        <input type="text" name="PositionName" value={statePositionChange} onChange={handlePositionChange} />
+                    </label>
+                    <label className="headerContents">
+                        Company Name:
+                        <input type="text" name="CompanyName" value={stateCompanyChange} onChange={handleCompanyChange} />
+                    </label>
+
+                    <label className="headerContents">
+                        Extra Comments:
+                        {/* <input type="text" name={stateCompanyName} onChange={handleCompanyChange} /> */}
+                        <textarea name="extraText" type="text" value={stateExtraComments} onChange={handleExtraCommentChange} id="" cols="50" rows="2"></textarea>
+                    </label>
+                    <input className="headerContents" type="submit" value="Submit" />
+                </form>
             </header>
             <body>
-                <p className="header">
+                <p>
                     New York Metropolitan Area, NY-11429<br />
                     (347) 600-4353<br />
                     saadat.taaseen@gmail.com<br />
@@ -73,6 +100,35 @@ function App() {
 }
 
 export default App;
+
+
+
+    // useEffect(() => {
+    //     let newPositionName = prompt('enter POSITION NAME')
+    //     let newCompanyName = prompt('enter COMPANY NAME')
+    //     // let confirmAdditional = confirm('Any Extra Comments you want to add after the first paragraph?')
+    //     // let newExtraComments = prompt('Any Extra Comments you want to add after the first paragraph?')
+    //     let newExtraComments;
+
+    //     if (window.confirm('Any Extra Comments you want to add after the first paragraph?')) {
+    //         newExtraComments = prompt('Type Additional Information in box')
+    //     }
+    //     else {
+    //         newExtraComments = null;
+    //     }
+
+    //     newCompanyName ? (setCompanyName(newCompanyName)) : (setCompanyName("COMPANY NAME"))
+    //     newPositionName ? (setPositionName(newPositionName)) : (setPositionName("POSITION NAME"))
+    //     newExtraComments ? (setExtraComments(newExtraComments)) : (setExtraComments(""));
+    // }, [])
+
+
+
+
+
+
+
+
 
 /*Saadat Taaseen
 
