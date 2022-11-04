bf4b785c1e89f688aae863cb54102224cf44ea26
taaseen71 <dev.saadat@gmail.com>
2022-11-04 19:49:28 -0400

    MUI installed

---
+++ src/Body.js
@@ -0,0 +1,151 @@
+import React, {useState} from 'react'
+import CopyToClip from './components/CopyToClip'
+import CreateExcelTabs from './components/CreateExcelTabs';
+import Content from './components/Content.jsx';
+import { TextField, Typography, Button } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+
+
+const Body = ({setBackgroundColor, background}) => {
+    
+    const [todayDate] = useState(new Date().toLocaleString().split(","))
+    const [statePositionChange, setStatePositionChange] = useState("")
+    const [stateCompanyChange, setStateCompanyChange] = useState("")
+    const [stateLinkChange, setStateLinkChange] = useState("")
+    const [stateExtraComments, setStateExtraComments] = useState("")
+    
+    const [positionName, setPositionName] = useState("POSITION NAME");
+    const [companyName, setCompanyName] = useState("COMPANY NAME");
+    const [link, setLink] = useState('')
+    const [extraComments, setExtraComments] = useState("");
+
+    const [copiedToClipboard, setCopiedToClipboard] = useState({
+        value: '',
+        copied: false,
+    });
+    const grid = [
+        [{ value: positionName }, { value: companyName }, { value: 'Link', expr: link }],
+    ]
+
+        
+    const handlePositionChange = (event) => {
+        setStatePositionChange(event.target.value)
+    }
+    const handleCompanyChange = (event) => {
+        setStateCompanyChange(event.target.value)
+    }
+
+    const handleLinkChange = (event) => {
+        setStateLinkChange(event.target.value)
+    }
+
+    const handleExtraCommentChange = event => {
+        setStateExtraComments(event.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setPositionName(statePositionChange)
+        setCompanyName(stateCompanyChange)
+        setLink(stateLinkChange)
+        setExtraComments(stateExtraComments)
+        //! Clear Defaults Below
+        setStatePositionChange("")
+        setStateCompanyChange("")
+        setStateExtraComments("")
+        setStateLinkChange("")
+      }
+    
+    const Restore = () => {
+        console.log(positionName, companyName, extraComments, link)
+            positionName && setStatePositionChange(positionName)
+            companyName && setStateCompanyChange(companyName)
+            link && setStateLinkChange(link)
+            companyName && setStateCompanyChange(companyName)
+            extraComments && setStateExtraComments(extraComments)
+    }
+    
+  return (
+    <div>
+        <div>
+            <Button variant="outlined" color="secondary" onClick={() => setBackgroundColor()}>DarkMode: {background.darkMode}</Button>
+            <header className="header">
+            <form onSubmit={handleSubmit} autoComplete="off">
+                <Typography variant='h2' className="headerContents">Saadat Taaseen </Typography>
+                
+                <label className="headerContents">
+                    <TextField 
+                        sx={{ input: { color: background.color}}} 
+                        id="outlined-basic" 
+                        label="Position Name" 
+                        fullWidth
+                        variant="outlined" 
+                        type="text" 
+                        value={statePositionChange} 
+                        onChange={handlePositionChange} 
+                    />
+                </label>
+
+                <label className="headerContents">
+                    <TextField 
+                        sx={{ input: { color: background.color}}} 
+                        id="outlined-basic" 
+                        label="Company Name" 
+                        variant="outlined" 
+                        fullWidth={true}
+                        type="text" 
+                        value={stateCompanyChange} 
+                        onChange={handleCompanyChange} 
+                    />
+                </label>
+
+                <label className="headerContents">
+                    <TextField 
+                        sx={{ input: { color: background.color}}} 
+                        id="outlined-basic" 
+                        label="Link To Job" 
+                        variant="outlined" 
+                        type="text" 
+                        value={stateLinkChange} 
+                        onChange={handleLinkChange} 
+                    />
+                </label>
+
+                <label className="headerContents">
+                    <TextField
+                        style={{textAlign: 'left'}}
+                        label="Extra Comments"
+                        multiline
+                        minRows={3}
+                        value={stateExtraComments} 
+                        onChange={handleExtraCommentChange}
+                    />
+                </label>
+              
+                {/* <input className="headerContents" type="submit" value="Submit" /> */}
+                <Grid container spacing={2}>
+                    <Grid xs={6}>
+                            <Button fullWidth={true} variant="contained" color="secondary" className="headerContents" type="submit" value="Submit">Submit</Button>
+                    </Grid>
+                    <Grid xs={6}>
+                            <Button fullWidth={true} variant="outlined" color="warning" onClick={Restore}>Restore</Button>
+                    </Grid>
+                </Grid>
+            </form>
+            </header>
+        </div>
+        <div>
+          <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'center', padding: '10px' }}>
+            {/* //* Create Excel Sheet */}
+            <CreateExcelTabs grid={grid} />
+            {/* //* COPY TO CLIPBOARD */}
+            <CopyToClip setCopiedToClipboard={setCopiedToClipboard} copiedToClipboard={copiedToClipboard} positionName={positionName} companyName={companyName} link={link} grid={grid} />
+          </div>
+          {/* //* Cover Letter and Interview Prompt */}
+          <Content positionName={positionName} companyName={companyName} extraComments={extraComments} todayDate={todayDate} />
+        </div>
+    </div>
+  )
+}
+
+export default Body
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import MailForInterviewRequest from './MailForInterviewRequest'
+// import { useStateIfMounted } from "use-state-if-mounted";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import axios from "axios";
+import {Button, Typography} from '@mui/material';
+
 
 function Content({ positionName, companyName, extraComments, todayDate }) {
-
-
   const [paragraph1, setParagraph1] = useState("")
   const [paragraph2, setParagraph2] = useState("")
   const [paragraph3, setParagraph3] = useState("")
@@ -16,7 +17,13 @@ function Content({ positionName, companyName, extraComments, todayDate }) {
   const [whyGoodFitAnswer, setWhyGoodFitAnswer] = useState("")
   
 
-  useEffect(() => {
+  useEffect(() => {   
+    GetFile()
+    return () => {
+    }
+  }, [extraComments, companyName, positionName])
+
+  const GetFile = () => {
     axios.get('/CoverLetterContent.json').then(response => {
       setParagraph1(`I would like to express my strong interest in your open ${positionName} position with ${companyName}.\u00A0` + (response.data.CoverLetterContent.paragraph1))
       setParagraph2(response.data.CoverLetterContent.paragraph2 + `I have exceeded multiple expectations from my current team, and want to be able to use my experiences to help reach all the goals that ${companyName} has. I am always looking to grow as an Engineer and looking forward to learning new languages and using my skill sets to benefit my peers.`)
@@ -27,11 +34,7 @@ function Content({ positionName, companyName, extraComments, todayDate }) {
       setWhatFreeTimeAnswer(response.data.misc.whatFreeTimeAnswer)
       setWhyGoodFitAnswer(response.data.misc.whyGoodFitAnswer)
     })
-
-    
-    return () => {
-    }
-  }, [extraComments, companyName, positionName])
+  }
   
 
 
@@ -80,7 +83,7 @@ function Content({ positionName, companyName, extraComments, todayDate }) {
           text={cover}
           onCopy={() => setCoverLetterClipboard({ ...coverLetterClipboard, copied: true })}
         >
-          <button>Copy Cover Letter</button>
+          <Button variant="contained">Copy Cover Letter</Button>
         </CopyToClipboard>
 
       </div> 
@@ -110,26 +113,26 @@ function Content({ positionName, companyName, extraComments, todayDate }) {
 function CoverLetter({ paragraph1, paragraph2, paragraph3, paragraph4, paragraph5, paragraph6, todayDate }) {
   return (
 <div className="content">
-<p><br/>
+<Typography variant="body1" gutterBottom><br/>
 {`New York Metropolitan Area, NY-10010`}<br/>
 {`+1 (540) 753-195`}<br/>
 {`dev.saadat@gmail.com`}<br/>
 {`https://www.linkedin.com/in/taaseen71/`}<br/>
 {`https://github.com/Taaseen71/`}<br/>
 {`https://saadt.netlify.app/`}<br/>
-</p>
-<p  className="todaysDate">{todayDate[0]}</p>
-<p>Dear Hiring Manager,<br /></p>
-<p>
+</Typography>
+<Typography variant="body1" gutterBottom  className="todaysDate">{todayDate[0]}</Typography>
+<Typography variant="body1" gutterBottom>Dear Hiring Manager,<br /></Typography>
+<Typography variant="body1" gutterBottom>
 {paragraph1}<br/>
 {paragraph2}<br/>
 {paragraph3}<br/>
 {paragraph4}<br/>
 {paragraph5}<br/>
 {paragraph6}
-</p>
-<p>Sincerely,< br /> 
-Saadat Taaseen</p>
+</Typography>
+<Typography variant="body1" gutterBottom>Sincerely,< br /> 
+Saadat Taaseen</Typography>
 </div>
   )
 }
